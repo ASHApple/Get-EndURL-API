d6243a5417546814d7ee68ac7c01ac1afd7e0d9f
ASHApple <admin@ashz.me>
2021-07-10 01:09:07 +0900

    Fix many bugs

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const request = require("request-promise-native");
+const axios = require("axios");
 const serverless = require('serverless-http');
+const moment = require("moment");
 
-app.get("/.netlify/functions/server", function(req, res) {
-  console.log(`${new Date()} | ${req.query.url ? req.query.url : "[NO URL]"}`);
+app.get("/.netlify/functions/index", async function(req, res) {
+  const timezone = new Date().getTimezoneOffset() / 60 * -1;
+  const time = `${moment().format("LLLL")} UTC${timezone > 0 ? "+" : ""}${timezone}`;
 
   if (!req.query.url) {
+    console.log(`${time}: ${req.ip} | [400] [NO URL]`);
+
     res.status(400);
     return res.json({
       error: true,
@@ -14,28 +18,31 @@ app.get("/.netlify/functions/server", function(req, res) {
     });
   }
 
-  const option = {
-    headers: {
-      "User-Agent": process.env.UserAgent
-    },
-    url: req.query.url,
-    followRedirect: true,
-    followAllRedirects: true,
-    maxRedirects: 20,
-    removeRefererHeader: true,
-    resolveWithFullResponse: true
-  };
+  try {
+    const response = await axios({
+      method: 'get',
+      headers: {
+        "Accept-Language": "en-US,en;q=0.5",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+      },
+      url: req.query.url,
+    });
 
-  request.get(option).then(result => {
+    console.log(response.request._redirectable._currentUrl);
+
     const obj = {
       error: false,
       message: null,
-      href: result.request.href,
-      host: result.request.host
+      href: response.request._redirectable._currentUrl,
+      host: new URL(response.request._redirectable._currentUrl).host
     };
-    return res.json(obj);
-  }).catch(err => {
-    console.error(err.stack);
+
+    console.log(`${time}: ${req.ip} | [200] ${req.query.url ? req.query.url : "[NO URL]"} → ${obj.href}`);
+
+    res.json(obj);
+  } catch (e) {
+    console.error(`${time}: ${req.ip} | [500] ${req.query.url ? req.query.url : "[NO URL]"}`);
+    console.error(e.stack);
 
     const obj = {
       error: true,
@@ -43,10 +50,11 @@ app.get("/.netlify/functions/server", function(req, res) {
       href: null,
       host: null
     };
+
     res.status(500);
     return res.json(obj);
-  });
+  }
 });
 
 module.exports = app;
-module.exports = serverless(app);
+module.exports.handler = serverless(app);
--- netlify.js
+++
@@ -1,5 +0,0 @@
-const main = require("./");
-
-main.listen(3000, () => {
-  console.log("EndURL Parser API is Running on port 3000");
-});
